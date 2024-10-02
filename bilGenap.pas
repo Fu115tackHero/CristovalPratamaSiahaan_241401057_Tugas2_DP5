b807661c4d697368fdf349121dad0bca3e274336
Fu115tackHero <pratamacristoval@gmail.com>
2024-10-02 08:37:01 +0700

    Add files via upload

---
+++ bilGenap.pas
@@ -0,0 +1,24 @@
+program bilGenap;
+uses crt;
+
+var
+    n, i : integer;
+
+begin
+clrscr;
+
+    write('Masukkan Batas: '); readln(n);
+
+    i := 1;
+    while (i <= n ) do
+    begin
+        
+        if (i mod 2 = 0) then
+        begin
+            write(i, ', ');
+        end;
+        
+        i := i + 1;
+    end;
+
+end.
